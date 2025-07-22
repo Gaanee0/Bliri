276b67a1a475fe2421977531f9da268eece500a4
Gaanee0 <ganeshhiyati007@gmail.com>
2025-07-22 08:53:58 +0530

    same line

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,9 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux helix
 #dnf5 install -y zellij
-dnf5 install -y helix
+#dnf5 install -y helix
 
 # Use a COPR Example:
 #
