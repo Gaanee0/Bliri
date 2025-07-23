a30dd2df0b849c5d393289bace541b88c0c5c24e
Gaanee0 <ganeshhiyati007@gmail.com>
2025-07-23 17:23:53 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,11 +8,6 @@ log() {
   echo "=== $* ==="
 }
 
-log "Creating temporary GPG directory..."
-export GNUPGHOME=/tmp/gnupg
-mkdir -p "$GNUPGHOME"
-chmod 700 "$GNUPGHOME"
-
 log "Enable COPR repos...." 
 COPR_REPOS=(
      
@@ -28,15 +23,6 @@ ADDITIONAL_SYSTEM_APPS=(
 log "Installing packages using dnf5..."
 dnf5 install --setopt=install_weak_deps=True -y \
 ${ADDITIONAL_SYSTEM_APPS[@]}
-
-log "Disable Copr repos to get rid of clutter..."
-for repo in "${COPR_REPOS[@]}"; do
-  dnf5 -y copr disable "$repo"
-done
-
-log "Cleaning up temporary GPG directory..."
-rm -rf "$GNUPGHOME"
-unset GNUPGHOME
 
 log "Enabling systemd.services..."
 systemctl enable podman.socket
