be5997852d5c8056059fa732062e9fdc015bd022
Gaanee0 <ganeshhiyati007@gmail.com>
2025-07-24 11:26:32 +0000

    terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,26 +8,24 @@ log() {
   echo "=== $* ==="
 }
 
-log "Add Terra repository..."
-dnf5 -y install --nogpgcheck --repofrompath "terra,https://repos.fyralabs.com/terra${RELEASE}" terra-release
-dnf5 -y install terra-release-extras || true
-dnf5 config-manager setopt "terra*".enabled=0
-
 log "Enable COPR repos...." 
 COPR_REPOS=(
-    yalter/niri
+    
 )
 for repo in "${COPR_REPOS[@]}"; do
   dnf5 -y copr enable "$repo"
 done
 
-ADDITIONAL_SYSTEM_APPS=(
+log "Enable terra repositories..."
+dnf5 config-manager setopt terra.enabled=1 terra-extras.enabled=1
+
+ADDITIONAL_APPS=(
      wpaperd
 ) 
 
 log "Installing packages using dnf5..."
 dnf5 install --setopt=install_weak_deps=True -y \
-${ADDITIONAL_SYSTEM_APPS[@]}
+${ADDITIONAL_APPS[@]}
 
 log "Disable Copr repos to get rid of clutter..."
 for repo in "${COPR_REPOS[@]}"; do
@@ -35,4 +33,3 @@ for repo in "${COPR_REPOS[@]}"; do
 done
 
 log "Enabling systemd.services..."
-systemctl enable podman.socket
