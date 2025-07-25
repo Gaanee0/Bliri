d7f21c0d2c7f16fab68963e54229955012446f8b
Gaanee0 <ganeshhiyati007@gmail.com>
2025-07-25 14:31:42 +0530

    Niri pkgs

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,10 @@ log() {
 
 log "Enable COPR repos...." 
 COPR_REPOS=(
-    
+    tofik/sway
+    ulysg/xwayland-satellite
+    yalter/niri
+    leloubil/wl-clip-persist
 )
 for repo in "${COPR_REPOS[@]}"; do
   dnf5 -y copr enable "$repo"
@@ -20,12 +23,44 @@ log "Enable terra repositories..."
 dnf5 config-manager setopt terra.enabled=1 terra-extras.enabled=1
 
 ADDITIONAL_APPS=(
-     wpaperd
-) 
+    kitty
+    kitty-terminfo
+    dolphin
+    filelight
+    kde-connect
+    btrfs-assistant
+)
+
+NIRI_PKGS=(
+    niri
+    wpaperd
+    swaylock
+    swayidle
+    brightnessctl
+    fuzzel
+    mako
+    waybar
+    xwayland-satellite
+    gnome-keyring
+    wireplumber
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal-gtk
+    plasma-polkit-agent
+    polkit-kde
+    cliphist
+    wl-clipboard
+    blueman
+    wl-clip-persist
+    wlogout
+    wireplumber
+    wl-copy
+    network-manager-applet
+)
 
 log "Installing packages using dnf5..."
 dnf5 install --setopt=install_weak_deps=True -y \
-${ADDITIONAL_APPS[@]}
+${ADDITIONAL_APPS[@]} \
+${NIRI_PKGS[@]}
 
 log "Disable Copr repos to get rid of clutter..."
 for repo in "${COPR_REPOS[@]}"; do
