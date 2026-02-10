63430e6506ad8432b820d6f6315fe51d71c140a2
Gaanee <ganeshhiyati007@gmail.com>
2026-02-10 20:12:35 +0530

    niri

--- build_files/build.sh
+++ build_files/build.sh
@@ -115,3 +115,4 @@ rm -rvf /usr/share/wayland-sessions/gamescope-session.desktop
 rm -rvf /usr/share/wayland-sessions/plasma-steamos-wayland-oneshot.desktop
 rm -rvf /usr/share/xsessions/plasma-steamos-oneshot.desktop
 dnf5 clean all
+#
