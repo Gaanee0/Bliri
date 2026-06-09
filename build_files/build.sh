a5192cd8f5318efdede387b4be1e5aa99a30d19e
Gaanee <ganeshhiyati007@gmail.com>
2026-06-10 00:42:46 +0530

    Fix: Darkly

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,6 +14,7 @@ COPR_REPOS=(
   avengemedia/dms-git
   gaanee/libfprint-elanmoc2
   deltacopy/darkly
+  scottames/ghostty
 )
 for repo in "${COPR_REPOS[@]}"; do
   dnf5 -y copr enable "$repo"
@@ -27,8 +28,9 @@ curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo \
 # Repo priorities (lower = higher priority)
 echo "priority=1" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 echo "priority=1" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:ulysg:xwayland-satellite.repo
-echo "priority=2" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:avengemedia:danklinux.repo
-echo "priority=2" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:deltacopy:darkly.repo
+echo "priority=1" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:scottames:ghostty.repo
+echo "priority=2" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:avengemedia:danklinux.repo >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
+echo "priority=2" >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:deltacopy:darkly.repo >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo >>/etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 dnf5 -y config-manager setopt terra.enabled=1 "*terra*".priority=3
 
 # ── Packages ───────────────────────────────────────────────────────────────────
